7ff1536ae4a631483900dffe70b607bc5caba749
Thiago <tcaprodutoradigital@gmail.com>
2026-03-20 06:47:43 -0300

    corrige chave Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
-/* src/supabaseClient.js */
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelos seus dados do Supabase (Settings -> API)
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://cwoyhgiwgufsipmuqsik.supabase.co';
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY || 'sb_publishable_KVbAj900jy2wl7KIvJQkJg_KN2m_D_J';
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b3loZ2l3Z3Vmc2lwbXVxc2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTQ3MzYsImV4cCI6MjA4ODk5MDczNn0.CioKepsqgBiUJc7HHrtAcd2eRRTUrZzr79xZjIP2J_U';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
